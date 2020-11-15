16f0f7c178e6b12190c5142d7cf226ee3253ff86
nemchinovrp <nemchinovrp@yandex.ru>
2020-11-15 22:44:24 +0800

    added morgan and cors

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const authRoutes = require('./routes/auth')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
 const app = express()
 
+app.use(morgan('dev', ''))
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
